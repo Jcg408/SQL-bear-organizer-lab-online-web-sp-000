2b596fbdd56c6122cb614b24e8366f2833f5f3b7
Jessica Gladstone <jcg321@comcast.net>
2019-01-20 22:05:34 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,3 +2,24 @@ INSERT INTO bears (
   "Mr. Chocolate", 12, "male", "brown", "mellow", true
 );
 
+INSERT INTO bears (
+  "Rowdy", 20, "male", "black", "aggressive", true
+);
+INSERT INTO bears (
+  "Tabitha", 5, "female", "brown", "friendly", true
+);
+INSERT INTO bears (
+  "Sergeant Brown", 35, "male", "grey", "aggressive", false
+);
+INSERT INTO bears (
+  "Melissa", 22, "female", "black", "mellow", true
+);
+INSERT INTO bears (
+  "Grinch", 9, "male", "grey", "independent", true
+);
+INSERT INTO bears (
+  "Wendy", 14, "female", "white", "friendly", true
+);
+INSERT INTO bears (
+  NULL, NULL, "male", "black", "aggressive", false
+)
