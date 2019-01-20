a0e58f8d32ebb0049d1d9696ddb0d6ab30079bea
Jessica Gladstone <jcg321@comcast.net>
2019-01-20 22:15:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,10 +1,10 @@
 INSERT INTO bears 
- VALUES ("Mr. Chocolate", 12, "male", "brown", "mellow", 0);
+ VALUES (1,"Mr. Chocolate", 12, "male", "brown", "mellow", 0);
 
-INSERT INTO bears VALUES ("Rowdy", 20, "male", "black", "aggressive", 0);
-INSERT INTO bears VALUES ("Tabitha", 5, "female", "brown", "friendly", 0);
-INSERT INTO bears VALUES ("Sergeant Brown", 35, "male", "grey", "aggressive", 1);
-INSERT INTO bears VALUES ("Melissa", 22, "female", "black", "mellow", 0);
-INSERT INTO bears VALUES ("Grinch", 9, "male", "grey", "independent", 0);
-INSERT INTO bears VALUES ("Wendy", 14, "female", "white", "friendly", 0);
-INSERT INTO bears VALUES (NULL, 5, "male", "black", "aggressive", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 20, "male", "black", "aggressive", 0);
+INSERT INTO bears VALUES (3,"Tabitha", 5, "female", "brown", "friendly", 0);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 35, "male", "grey", "aggressive", 1);
+INSERT INTO bears VALUES (5,"Melissa", 22, "female", "black", "mellow", 0);
+INSERT INTO bears VALUES (6,"Grinch", 9, "male", "grey", "independent", 0);
+INSERT INTO bears VALUES (7,"Wendy", 14, "female", "white", "friendly", 0);
+INSERT INTO bears VALUES (8,NULL, 5, "male", "black", "aggressive", 1);
